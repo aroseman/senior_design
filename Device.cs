5f1142430fc799e010760a21096bda1f09b2f433
mgj5046 <33975379+mgj5046@users.noreply.github.com>
2018-01-15 20:44:22 -0500

    Device.cs

---
+++ Device.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Device
+{
+    public string Name { get; set; }
+
+    public string IpAddress { get; set; }
+
+    public string Description { get; set; }
+
+    //1 is connected, 0 is disconnected
+    public bool Status { get; set; }
+
+    //Contains Neighboring IP Addresses
+    public List<Device> Neighbors {get; set; }
+
+    public Device()
+    {
+        Name = string.Empty;
+        IpAddress = string.Empty;
+        Description = string.Empty;
+        Status = false;
+        Neighbors = new List<Device>();
+    }
+}
